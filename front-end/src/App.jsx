1afa2d62e02380801aed678ac2c6796baffec434
rachel2bester <rachel2bester@gmail.com>
2022-07-27 15:37:12 +0100

    added routing

--- front-end/src/App.jsx
+++ front-end/src/App.jsx
@@ -2,13 +2,24 @@ import logo from './logo.svg';
 import './App.css';
 import Home from './containers/Home/Home';
 import NavBar from './components/NavBar/NavBar.jsx'
+import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import NewCourse from './containers/NewCourse/NewCourse';
+import Course from './containers/Course/Course';
 
 function App() {
+  const baseURL = "http://localhost:8080/"
   return (
-    <div className="App">
-      <NavBar />
-      <Home />
-    </div>
+    <Router>
+      <div className="App">
+        <NavBar/>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/create-course" element={<NewCourse baseURL={baseURL}/>}/>
+          <Route path="/course/:courseId" element={<Course baseURL={baseURL}/>}/>
+        </Routes>
+        
+      </div>
+  </Router>
   );
 }
 
--- front-end/src/containers/Home/Home.jsx
+++ front-end/src/containers/Home/Home.jsx
@@ -1,16 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import CourseCard from '../../components/CourseCard/CourseCard';
 
 
 const Home = () => {
-    const url = "";
-    const getCoursesJSX = async () => {
+    const [courses, setCourses] = useState([]);
+    const [responseOK, setResponseOK] = useState(undefined);
 
+    const url = "http://localhost:8080/courses";
+    const getCourses = async () => {
+        const response = await fetch(url);
+        let data = await response.json();
+        setResponseOK(response.ok);
+        setCourses(data);
     }
+
+    useEffect(() => {
+        getCourses();
+    }, [])
+
+    let coursesJSX = courses.map((course) => {
+        const {name, category, completionTime, price, description, author} = course;
+        return <CourseCard name={name} category={category} completionTime={completionTime} price={price} author={author} description={description}/>
+    })
+
     return (
         <div className='home'>
             <h1>All Courses</h1>
-            <CourseCard name="course" category="category" completionTime={7} price={43.99} author="author" description={""}/>
+            {coursesJSX}
         </div>
     )
 }
